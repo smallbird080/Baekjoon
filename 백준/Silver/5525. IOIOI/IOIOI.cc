ec0b22b807f64087aaaaf3ff957a4521acae611f
smallbird080 <102128593+smallbird080@users.noreply.github.com>
2024-03-10 03:42:28 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,45 +1,24 @@
 #include <iostream>
 #include <string>
 
-std::string getPattern(int n){
-    std::string base = "IOI";
-    for (int i=1; i<n; i++){
-        base = base+"OI";
-    }
-    return base;
-}
-
-
-
 int main(){
     int n,m,cnt = 0;
     std::string s;
     std::cin>>n>>m>>s;
-    std::string pattern = getPattern(n);
     int index = 0;
     while (index<m){
+        int numOI = 0;
         if (s[index] == 'I'){
-            int i = 0;
-            int tempIndex = index;
-            while(i<(int)(pattern.size()) && s[tempIndex] == pattern[i]){
-                i++;
-                tempIndex++;
-            }
-            if(i==(int)(pattern.size())){
+            while (s[index+1]=='O' && s[index+2]=='I'){
+                numOI++;
                 index +=2;
-                cnt++;
-                continue;
+                if(numOI==n){
+                    cnt++;
+                    numOI--;
+                }
             }
-            else{
-                index = tempIndex;
-                continue;
-            }
-
         }
-        else{
-            index++;
-            continue;
-        }
+        index++;
     }
     std::cout<<cnt;
 }
