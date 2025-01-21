2c3c17218bf0a63163e65858b5a7afa8a1790b40
smallbird080 <102128593+smallbird080@users.noreply.github.com>
2025-01-22 04:45:44 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -10,7 +10,7 @@ int main() {
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
-    for (int i=4; i<12; i++){
+    for (int i=4; i<11; i++){
         dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
     }
     cin >> n;
