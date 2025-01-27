a259bec8ea3d804ebcdb7502868e0100f8d7d921
smallbird080 <kobi200404@gmail.com>
2025-01-28 01:03:07 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2288 KB - deque 풀이로 수정정

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -9,29 +9,29 @@ int main() {
     int n,k;
     cin >> n >> k;
     vector<bool> visited(100001,false);
-    queue<pair<int,int>> pos;
-    pos.push({n,0});
+    deque<pair<int,int>> pos;
+    pos.push_front({n,0});
     visited[n] = true;
     while (!pos.empty()){
         int idx = pos.front().first;
         int dist = pos.front().second;
-        pos.pop();
+        pos.pop_front();
         if (idx==k){
             cout << dist;
             break;
         }
         if (idx*2 <= 100000 && !visited[idx*2]){
             visited[idx*2] = true;
-            pos.push({idx*2,dist});
+            pos.push_front({idx*2,dist});
         }
 
         if (idx > 0 && !visited[idx-1]){
             visited[idx-1] = true;
-            pos.push({idx-1,dist+1});
+            pos.push_back({idx-1,dist+1});
         }
         if (idx < 100000 && !visited[idx+1]){
             visited[idx+1] = true;
-            pos.push({idx+1,dist+1});
+            pos.push_back({idx+1,dist+1});
 
         }
     }
