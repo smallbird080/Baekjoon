de4a6c85201005fb12f0feedc5f11834e17a55db
smallbird080 <102128593+smallbird080@users.noreply.github.com>
2025-01-27 22:47:23 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,22 +9,14 @@ int main() {
     int n;
     cin >> n;
     int current[3] = {0,0,0};
-    vector<int> BR(n+1,0);
-    vector<int> GR(n+1,0);
-    vector<int> BG(n+1,0);
-    vector<int> RG(n+1,0);
-    vector<int> RB(n+1,0);
-    vector<int> GB(n+1,0);
+    vector<int> R(n+1,0);
+    vector<int> G(n+1,0);
+    vector<int> B(n+1,0);
     for (int i=1; i<=n; i++){
         cin >> current[0] >> current[1] >> current[2];
-        BR[i] = min(RB[i-1]+current[0], GB[i-1]+current[0]);
-        GR[i] = min(RG[i-1]+current[0], BG[i-1]+current[0]);
-        BG[i] = min(RB[i-1]+current[1], GB[i-1]+current[1]);
-        RG[i] = min(BR[i-1]+current[1], GR[i-1]+current[1]);
-        RB[i] = min(BR[i-1]+current[2], GR[i-1]+current[2]);
-        GB[i] = min(RG[i-1]+current[2], BG[i-1]+current[2]);
+        R[i] = min(G[i-1]+current[0],B[i-1]+current[0]);
+        G[i] = min(R[i-1]+current[1],B[i-1]+current[1]);
+        B[i] = min(R[i-1]+current[2],G[i-1]+current[2]);
     }
-
-    int result = min({BR[n], GR[n], BG[n], RG[n], RB[n], GB[n]});
-    cout << result;
+    cout << min({R[n],B[n],G[n]});
 }
