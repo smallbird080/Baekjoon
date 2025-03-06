f70151a061ff4d0066b99bc0afaaf92c778241ef
smallbird080 <102128593+smallbird080@users.noreply.github.com>
2025-03-06 15:21:21 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 208 ms, Memory: 26356 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+#define IO ios::sync_with_stdio(false);cin.tie(NULL)
+
+using namespace std;
+
+void printLIS(const vector<int>& arr, const vector<int>& parent, int idx) {
+    vector<int> lis;
+    for (; idx >= 0; idx = parent[idx])
+        lis.push_back(arr[idx]);
+    reverse(lis.begin(), lis.end());
+    for (int val : lis)
+        cout << val << " ";
+    cout << "\n";
+}
+
+int main() {
+    IO;
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    vector<int> lis;
+    vector<int> parent(n, -1);
+    vector<int> lis_indices;
+
+    for (int i = 0; i < n; i++) {
+        auto it = lower_bound(lis.begin(), lis.end(), arr[i]);
+        int idx = distance(lis.begin(), it);
+
+        if (it == lis.end()) {
+            lis.push_back(arr[i]);
+            lis_indices.push_back(i);
+        } else {
+            *it = arr[i];
+            lis_indices[idx] = i;
+        }
+
+        if (idx > 0)
+            parent[i] = lis_indices[idx - 1];
+    }
+
+    cout << lis.size() << "\n";
+    printLIS(arr, parent, lis_indices.back());
+
+    return 0;
+}
