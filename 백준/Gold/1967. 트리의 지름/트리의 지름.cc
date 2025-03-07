dc3bdb5e0baac77f1893c3a03a771e9fc9483644
smallbird080 <102128593+smallbird080@users.noreply.github.com>
2025-03-07 18:47:51 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2868 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#define IO ios::sync_with_stdio(false);cin.tie(NULL)
+
+using namespace std;
+
+struct Node{
+    vector<pair<int,int>> vertex;
+    bool visited = false;
+    int dist=0;
+
+    void reset(){
+        this->visited = false;
+        this->dist = 0;
+    }
+};
+
+pair<int,int> bfs(vector<Node>& tree, int root){
+    int maxLen = 0;
+    int maxVert = 0;
+    queue<int> q;
+    q.emplace(root);
+    tree[root].visited = true;
+    while (!q.empty()){
+        int cur = q.front();
+        q.pop();
+        int childLen = tree[cur].vertex.size();
+        if (childLen==1 && cur != root){
+            if (tree[cur].dist > maxLen){
+                maxLen = tree[cur].dist;
+                maxVert = cur;
+            }
+            continue;
+        }
+        for (int i=0; i<childLen; i++){
+            int v = tree[cur].vertex[i].first;
+            if (!tree[v].visited){
+                q.push(v);
+                tree[v].dist += tree[cur].dist + tree[cur].vertex[i].second;
+                tree[v].visited = true;
+            }
+        }
+    }
+    return {maxVert,maxLen};
+}
+
+int main() {
+    IO;
+    int n;
+    cin >> n;
+    vector<Node> Tree(n+1);
+    for (int i=0; i<n-1; i++){
+        int u,v,w;
+        cin >> u >> v >> w;
+        Tree[u].vertex.emplace_back(v,w);
+        Tree[v].vertex.emplace_back(u,w);
+    }
+    int far = bfs(Tree,1).first;
+    for (int i=1; i<=n; i++){
+        Tree[i].reset();
+    }
+    cout << bfs(Tree,far).second;
+}
