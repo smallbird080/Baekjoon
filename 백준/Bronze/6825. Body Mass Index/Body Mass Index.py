4eae24c65b9e2e39ab3423061267946c0533d281
smallbird080 <102128593+smallbird080@users.noreply.github.com>
2025-04-01 17:00:42 +0900

    [Bronze IV] Title: Body Mass Index, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.py
@@ -0,0 +1,9 @@
+weight = float(input())
+height = float(input())
+bmi = weight / (height ** 2)
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 <= bmi <= 25:
+    print("Normal weight")
+else:
+    print("Overweight")
